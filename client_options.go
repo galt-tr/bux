048f7e60977d09b2c9f7c437e4a3703bb9e73a70
galt-tr <dylan@truereviews.io>
2022-05-13 22:51:37 -0400

    Change to migrateModels

--- client_options.go
+++ client_options.go
@@ -480,7 +480,7 @@ func WithPaymailSupport(domains []string, defaultFromPaymail, defaultNote string
 		}
 
 		// Add the paymail_address model in bux
-		c.addModels(modelList, &PaymailAddress{Model: *NewBaseModel(ModelPaymailAddress)})
+		c.addModels(migrateList, &PaymailAddress{Model: *NewBaseModel(ModelPaymailAddress)})
 	}
 }
 
